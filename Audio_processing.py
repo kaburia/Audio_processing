ff8ff0b0ed55869601681399c04f7dc54ea8b1f0
kaburia <kaburiaaustin1@gmail.com>
2022-10-14 06:46:26 +0300

    time domain and spectrogram

--- Audio_processing.py
+++ Audio_processing.py
@@ -1,6 +1,37 @@
 import os
 import random
 import matplotlib.pyplot as plt
-import librosa
+import numpy as np
 
-audio_list = random.choice(os.listdir('2020-11-14'))
+import librosa as lr
+from librosa.display import specshow
+from librosa.core import stft, amplitude_to_db
+
+
+dir = '2020-11-14/'
+
+def audio(dir):
+    return random.choice(os.listdir(dir))
+
+acoustic_audio, freq = lr.load(dir+audio(dir))
+
+def time_domain():
+    ixs = np.arange(acoustic_audio.shape[-1])
+    time = ixs/freq
+    fig, ax  = plt.subplots()
+    ax.plot(time, acoustic_audio)
+    plt.show()
+
+def spectogram():
+    HOP_LENGTH = 2**4
+    SIZE_WINDOW = 2**7
+    audio_spec = stft(acoustic_audio, hop_length=HOP_LENGTH, n_fft=SIZE_WINDOW)
+    # Convert into decibels for visualization
+    spec_db = amplitude_to_db(audio_spec)# Visualize
+    fig, ax = plt.subplots()
+    specshow(spec_db, sr=freq, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH,  ax=ax)
+    plt.show()
+
+print(spectogram())
+
+
